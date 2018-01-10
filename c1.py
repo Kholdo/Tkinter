17b9aef6a1bfceec1dd8069a4fd2c3b10b2f6514
Koldo Pina <koldopinaortiz@gmail.com>
2018-01-10 18:02:49 +0100

    Initial commit

---
+++ c1.py
@@ -0,0 +1,10 @@
+#importamos la libreria
+import tkinter as tk
+#Creamos una instancia de la clase TK. Asi se crea la root window
+root = tk.Tk()
+#Añadimos algunos widgets
+frame = tk.Frame(root)
+frame.pack()
+
+#ejecutamos el metodo mainloop, que es el que mantiene la ventana visible
+root.mainloop()
